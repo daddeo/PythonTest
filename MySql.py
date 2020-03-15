50a55c50e274aba6a3edd61d0b742b38e603a849
daddeo <jason.wallenfang@gmail.com>
2020-03-15 00:07:34 -0500

    initial

---
+++ MySql.py
@@ -0,0 +1,23 @@
+# install the MySQL connector with PyCharm
+# 1. open command prompt
+# 2. run "pip3 install mysql-connector"
+#
+# current as of now is v2.2.9
+
+import mysql.connector
+
+db = mysql.connector.connect(host="localhost", user="{username}", passwd="{password}", database="{name}")
+
+# show all available databases
+cursor = db.cursor()
+cursor.execute("show databases")
+for i in cursor:
+    print(i)
+
+# instead of using cursor can just use the result
+result = cursor.fetchall() # fetch all rows
+print(result)
+
+result = cursor.fetchone() # fetch one row
+print(result)
+
---
+++ Sort.py
@@ -0,0 +1,23 @@
+
+def selectionSort(list, showProgress):
+    for i in range(len(list)-1):
+        minpos = i
+        for j in range(i,len(list)):
+            if list[j] < list[minpos]:
+                minpos = j
+
+        temporary = list[i]
+        list[i] = list[minpos]
+        list[minpos] = temporary
+
+        if showProgress:
+            if minpos == i:
+                print(list, " -- no change")
+            else:
+                print(list, " -- swapped {} and {}".format(list[i], list[minpos]))
+
+numbers = [5, 3, 8, 7, 6, 2]
+selectionSort(numbers, True)
+
+print(numbers)
+
