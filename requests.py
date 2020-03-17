f47b6a267c7880a3781026b375d3870ad224f691
daddeo <jason.wallenfang@gmail.com>
2020-03-17 14:14:38 -0500

    blah

---
+++ requests.py
@@ -0,0 +1,9 @@
+# install the MySQL connector with PyCharm
+# 1. open command prompt
+# 2. run "pip install requests"
+#
+# dependencies: certifi, urllib3, idna, chardet
+# current as of now is v2.23.0
+
+import requests
+
